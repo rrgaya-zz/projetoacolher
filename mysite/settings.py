3c3199ddb5c5ca5b00a46519d8afb81c3e179fa1
thiagojoa <thiago.msbueno@gmail.com>
2019-06-10 19:25:51 -0300

    Updated settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -139,4 +139,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
-DATABASES['default'] = dj_database_url.config(default=dbinfo)
