a44410ee36cef1635105e7e7e60032896107d0d2
thiagojoa <thiago.msbueno@gmail.com>
2019-06-10 19:05:27 -0300

    Set DB

--- mysite/settings.py
+++ mysite/settings.py
@@ -1,3 +1,13 @@
+import django_heroku
+
+...
+
+# All of your settings here
+
+...
+
+django_heroku.settings(locals())
+
 """
 Django settings for mysite project.
 
@@ -119,5 +129,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+#STATIC_URL = '/static/'
+#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
